3bf0ac5761d27f45110bee3f3fd908b7e96bfaf7
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-09-17 12:09:33 +0300

    feat

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,7 @@
 
 // import { DB_PORT, DB_DOMAIN } from '@/config/constants';
 // const database_url = `http://${DB_DOMAIN}:${DB_PORT}`;
-const database_url = `http://jvfr-jvbk:8080`;
+const database_url = `http://localhost:8080`;
 
 const fetchProducts = async () => {
   const response = await fetch(`${database_url}/products`);
