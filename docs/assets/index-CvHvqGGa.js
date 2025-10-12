8a10166b2d8ffc1a3cc03b770fef69559bd5d082
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-10-12 18:31:27 +0300

    feat

--- docs/assets/index-CvHvqGGa.js
+++
@@ -1,4 +0,0 @@
-import{R as n}from"./index-CHj3fuIg.js";const i=`<div class="landing">
-  <a href="/" class="inLink">To home</a>
-</div>
-`,o='@charset "UTF-8";';window.on(n,s=>{});export{o as css,i as html};
--- docs/assets/index-DdN3k4kM.js
+++
@@ -1,4 +0,0 @@
-import{R as n}from"./index-BQes1ajs.js";const o=`<div class="landing">
-  <a href="/" class="inLink">To home</a>
-</div>
-`,a="";window.on(n,s=>{});export{a as css,o as html};
