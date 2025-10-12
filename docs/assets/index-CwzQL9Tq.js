9ebcf2a604b093afbe4957a5f63f30a148f12184
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-10-12 17:35:17 +0300

    build

---
+++ docs/assets/index-CwzQL9Tq.js
@@ -0,0 +1,15 @@
+import{R as e}from"./index-CHj3fuIg.js";const n=`<div class="error">
+  <div class="error__image">
+    <span class="error--telescope emoji--mono">🔭 </span>
+    <!-- <span class="error--cross emoji--mono"> ✖︎</span> -->
+  </div>
+  <p class="error__code">✖︎ Помилка 404</p>
+  <p class="error__message">Сторінку не знайдено</p>
+
+  <a href="/" class="error__homeLink inLink emoji--mono">
+    Повернутись на головну</a
+  >
+</div>
+
+<!-- <span class="erorr--telescope emoji--mono"> ✖︎🔭 🚧 </span> -->
+`,s='@charset "UTF-8";.error{display:flex;gap:2rem;flex-flow:column nowrap;justify-content:center;align-items:center;padding:5rem}.error__image{display:flex;justify-content:center;align-items:center;position:relative;width:200px;height:200px;border-radius:50%;background:#848482}.error--telescope{font-size:7rem;color:#fff}.error__message{font-size:3rem}.error__code{font-size:1.5rem;color:#ff3131}.error__homeLink{font-size:1.5rem}';window.on(e,r=>{});export{s as css,n as html};
