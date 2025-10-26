97ed51aea4c1f12b3f337e3e43a34d8e9156d17f
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-10-26 09:36:20 +0200

    feat

--- src/components/carBatteryCard.js
+++ src/components/carBatteryCard.js
@@ -55,5 +55,6 @@ const createCarBatteryCard = (p) => {
 // remove duplicates that have only solely different polarity, set polarity to `null` in such objects. please pay attention to sizes of car battery, not to remove json object with same polarity, but different sizes
 // please sort it now by capacity, but same time for same capacity by price
 //set "low" tag on those who have 175 height
+//return result in json
 
 export default createCarBatteryCard;
